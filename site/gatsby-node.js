de19edc26db00415822ea3f6b3b11d9bc0923dd8
Jeff Kahlina <jkahlina@gmail.com>
2018-10-24 22:37:35 -0600

    Remove styled-components workaround

--- site/gatsby-node.js
+++ site/gatsby-node.js
@@ -7,16 +7,6 @@
 const path = require('path')
 const { createFilePath } = require(`gatsby-source-filesystem`)
 
-exports.onCreateBabelConfig = ({ stage, actions }, pluginOptions) => {
-  const ssr = stage === `build-html` || stage === `build-javascript`
-
-  actions.setBabelPlugin({
-    name: `babel-plugin-styled-components`,
-    stage,
-    options: { ...pluginOptions, ssr },
-  })
-}
-
 exports.onCreateNode = ({ node, getNode, actions }) => {
   const { createNodeField } = actions
 
